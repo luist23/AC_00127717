52f08dc55663cf590d03b4d82476bd1efcf5be22
luist23 <00127717@uca.edu.sv>
2020-04-26 20:00:25 -0600

    test taea 02 actividad 1 calcualo

--- tarea_02_00127717.asm
+++ tarea_02_00127717.asm
@@ -1,2 +1,14 @@
 ; parte I   : 00127717 -> (2+7+7+1+7)/5 = 4.8 -> 5: En el segundo
 
+org     100h
+        mov     ax, 0000h
+        add     ax, 2d
+        add     ax, 7d
+        add     ax, 7d
+        add     ax, 1d
+        add     ax, 7d
+        mov     cl, 5d
+        div     cl
+
+
+int 20h
