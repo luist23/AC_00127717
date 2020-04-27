b2939a566961a5c06258a9f2bb171be0c1728569
luist23 <00127717@uca.edu.sv>
2020-04-26 20:23:16 -0600

    quemando mensaje

--- tarea_02_00127717.asm
+++ tarea_02_00127717.asm
@@ -11,4 +11,34 @@ org     100h
         div     cl
 
 
+        mov     cl, "E"
+        mov     [200h], cl
+
+        mov     cl, "n"
+        mov     [201h], cl
+        mov     [20Ah], cl
+        
+        mov     cl, " "
+        mov     [202h], cl
+        mov     [205h], cl
+
+        mov     cl, "e"
+        mov     [203h], cl
+        mov     [207h], cl
+
+        mov     cl, "l"
+        mov     [204h], cl
+        mov     cl, "s"
+        mov     [206h], cl
+        mov     cl, "g"
+        mov     [208h], cl
+        mov     cl, "u"
+        mov     [209h], cl
+        mov     cl, "d"
+        mov     [20Bh], cl
+        mov     cl, "o"
+        mov     [20Ch], cl
+        
+
+
 int 20h
